d5aafd292218c7c057a8742e628ae0590e0d3102
Ashwin Sudhakar <ashwin.sudhakar@aethanalytica.com>
2021-10-11 15:46:08 +0530

    Second Commit

--- app/src/main/java/com/ashwin_sudhakar/myretrofitapplication/Helpers/ApiClient.java
+++ app/src/main/java/com/ashwin_sudhakar/myretrofitapplication/Helpers/ApiClient.java
@@ -2,6 +2,8 @@ package com.ashwin_sudhakar.myretrofitapplication.Helpers;
 
 import com.ashwin_sudhakar.myretrofitapplication.Interface.ApiInterface;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,10 +14,16 @@ public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static ApiInterface getApiClient(){
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
         if(retrofit == null){
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit.create(ApiInterface.class);
